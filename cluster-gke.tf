7e24f4445c9754e8c07d9d52ab906f6048321529
Mario Young <maye.co@gmail.com>
2020-03-28 15:29:55 -0300

    define alias ips

--- cluster-gke.tf
+++ cluster-gke.tf
@@ -24,6 +24,11 @@ resource "google_container_cluster" "cluster" {
     }
   }
 
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = "10.0.0.0/16"
+    services_ipv4_cidr_block = "10.2.0.0/20"
+  }
+
   timeouts {
     create = "30m"
     update = "30m"
