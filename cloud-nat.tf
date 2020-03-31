0ded3136ccdf85b0a10abb8736eb27cfdb052c94
Mario Young <maye.co@gmail.com>
2020-03-31 00:38:25 -0300

    add cloud nat

---
+++ cloud-nat.tf
@@ -0,0 +1,27 @@
+resource "google_compute_address" "cloudnat_ips" {
+  count   = 2
+  name    = "cloudnat-ip-${count.index}${var.name_sufix}"
+  region  = "us-central1"
+}
+
+resource "google_compute_router" "cloudnat_router" {
+  name    = "cloudnat-router"
+  region  = "us-central1"
+
+  bgp {
+    asn = 64514
+  }
+
+  depends_on = [google_compute_address.cloudnat_ips]
+}
+
+resource "google_compute_router_nat" "cloudnat" {
+  name                               = "cloudnat-0"
+  router                             = google_compute_router.cloudnat_router.name
+  region                             = "us-central1"
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  nat_ips                            = google_compute_address.cloudnat_ips.*.self_link
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  depends_on = [google_compute_router.cloudnat_router]
+}
