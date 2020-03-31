39fbaaaf6b18b73b5da459d3ccfe5102ea9a915c
Mario Young <maye.co@gmail.com>
2020-03-31 14:35:30 -0300

    add cloud sql instance

--- cloud-sql.tf
+++ cloud-sql.tf
@@ -15,3 +15,43 @@ resource "google_service_networking_connection" "servicenetworking_vpc_connectio
 
   depends_on = [google_compute_global_address.private_sql_ip_address]
 }
+
+resource "google_sql_database_instance" "cloud_sql" {
+  name             = "sql-us-central1"
+  region           = "us-central1"
+  database_version = "POSTGRES_11"
+
+  settings {
+    tier              = "db-custom-2-13312"
+    disk_type         = "PD_HDD"
+    disk_autoresize   = true
+    availability_type = "REGIONAL"
+
+    backup_configuration {
+      enabled            = true
+    }
+
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = "default"
+
+      authorized_networks {
+        name  = "cloudnat-ip-0"
+        value = google_compute_address.cloudnat_ips[0].address
+      }
+      authorized_networks {
+        name  = "cloudnat-ip-1"
+        value = google_compute_address.cloudnat_ips[1].address
+      }
+    }
+  }
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+  }
+
+  depends_on = [
+    google_service_networking_connection.servicenetworking_vpc_connection
+  ]
+}
