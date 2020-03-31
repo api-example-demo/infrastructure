56a203f9f7916666e93369e970a7cc2f1769aeb3
Mario Young <maye.co@gmail.com>
2020-03-31 14:24:21 -0300

    add internet auth network

--- cluster-gke.tf
+++ cluster-gke.tf
@@ -28,6 +28,13 @@ resource "google_container_cluster" "cluster" {
     cluster_ipv4_cidr_block  = "10.0.0.0/16"
     services_ipv4_cidr_block = "10.2.0.0/20"
   }
+  
+  master_authorized_networks_config {
+    cidr_blocks {
+      cidr_block   = "0.0.0.0/0"
+      display_name = "Internet"
+    }
+  }
 
   timeouts {
     create = "30m"
